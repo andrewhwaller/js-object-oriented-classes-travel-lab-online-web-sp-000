06c9fb4761cb1f0a7c7976dd7c521458949962df
Andrew Waller <andrewhwaller@gmail.com>
2019-09-11 22:54:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ class Driver {
     this.name = name;
     this.startDate = new Date(startDate);
   };
-
-  startDate() {
-    return Date.now()
-  };
-
+  
   yearsExperienceFromBeginningOf(year) {
     let endDate = new Date(year, 1, 1);
     let totalYears = (endDate - this.startDate) / (365 * 24 * 60 * 60 * 1000);
